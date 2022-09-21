7a06770300f6a32845e142446890c0239193d659
Carlos Roseno Matos <roseno@divulgueregional.com.br>
2022-09-21 11:56:53 -0300

    Revert "altered format api request"

--- lib/src/features/user/user_resource.dart
+++ lib/src/features/user/user_resource.dart
@@ -7,29 +7,29 @@ class UserResource extends Resource {
   @override
   List<Route> get routes => [
         Route.get('/user', _getAlluser),
-        // Route.get('/user:id', _getUserById),
+        Route.get('/user:id', _getUserById),
         Route.post('/user', _createUser),
         Route.put('/user', _updateUser),
-        Route.delete('/user:id', _deleteUser)
+        Route.delete('/user:id', _deleteUser),
       ];
 
   FutureOr<Response> _getAlluser() {
     return Response.ok('All user');
   }
 
-  // FutureOr<Response> _getUserById() {
-  //   return Response.ok('User:');
-  // }
-
-  FutureOr<Response> _createUser() {
-    return Response.ok('Create User:'); //recebe um json
+  FutureOr<Response> _getUserById(ModularArguments arguments) {
+    return Response.ok('User: ${arguments.params['id']}');
   }
 
-  FutureOr<Response> _updateUser() {
-    return Response.ok('Updated User:'); //recebe um json
+  FutureOr<Response> _createUser(ModularArguments arguments) {
+    return Response.ok('Create User: ${arguments.data}'); //recebe um json
   }
 
-  FutureOr<Response> _deleteUser() {
-    return Response.ok(' Deleted User:');
+  FutureOr<Response> _updateUser(ModularArguments arguments) {
+    return Response.ok('Updated User: ${arguments.data}'); //recebe um json
+  }
+
+  FutureOr<Response> _deleteUser(ModularArguments arguments) {
+    return Response.ok(' Deleted User: ${arguments.params['id']}');
   }
 }
